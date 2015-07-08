3dd77687f9ba60dbc09d94348b8b9ae1d0867391
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-07-08 16:53:41 +0900

    タップしてフロッピーディスクが上昇、PlayerPref によるハイスコア管理

---
+++ Assets/Floppy.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floppy : MonoBehaviour {
+	float FloppySpeed = -1;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			FloppySpeed = 20;
+		}
+		FloppySpeed -= 0.5f;
+		transform.Translate (transform.up * FloppySpeed);
+	
+	}
+}
