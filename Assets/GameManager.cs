94a8db3d2760a0d4b6b239a57a9329cc6b4c1dc1
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-07-08 16:59:06 +0900

    PlayerPrefによるハイスコア管理

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 
 public class GameManager : MonoBehaviour {
-	int score;
-
+	int CurrentScore = 0;
+	string HighScoreKey = "highScore"; 
+	public Text label;
+	 
 	// Use this for initialization
 	void Start () {
 	
@@ -12,6 +15,27 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		AddScore ();
 	
+	}
+
+	int GetHighScore() {
+		return PlayerPrefs.GetInt (HighScoreKey, 0);
+	}
+
+	void SetHighScore() {
+		PlayerPrefs.SetInt (HighScoreKey, CurrentScore);
+		PlayerPrefs.Save ();
+	}
+
+	void AddScore() {
+		CurrentScore++;
+		label.text = "Score: " + CurrentScore;
+	}
+
+	void OnColissionEnter2D(Collision2D collision) {
+		if (collision.gameObject.name == "Wall") {
+
+		}
 	}
 }
--- Assets/TitleManager.cs
+++ Assets/TitleManager.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class TitleManager : MonoBehaviour {
+	public Text label;
+	string HighScoreKey = "highScore";
+
 
 	// Use this for initialization
 	void Start () {
-	
+		label.text = "High Score: " + GetHighScore ();
 	}
 	
 	// Update is called once per frame
@@ -13,5 +17,9 @@ public class TitleManager : MonoBehaviour {
 		if (Input.GetMouseButtonDown (0)) {
 			Application.LoadLevel ("Game");
 		}	
+	}
+
+	int GetHighScore() {
+		return PlayerPrefs.GetInt (HighScoreKey, 0);
 	}
 }
