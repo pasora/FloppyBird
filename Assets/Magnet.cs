bbef154511ae10d2ed0a9e9c6a72633ac554d957
Masahiko Hara <pasora@sfc.wide.ad.jp>
2015-07-11 21:57:33 +0900

    マグネットが回転するようになった

--- Assets/Magnet.cs
+++ Assets/Magnet.cs
@@ -6,7 +6,8 @@ public class Magnet : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		GetComponent<Rigidbody2D>().velocity = new Vector2 (- speed, 0);
+		GetComponent<Rigidbody2D> ().velocity = new Vector2 (- speed, 0);
+		GetComponent<Rigidbody2D> ().AddTorque (200.0f, new ForceMode2D());
 	}
 	
 	// Update is called once per frame
